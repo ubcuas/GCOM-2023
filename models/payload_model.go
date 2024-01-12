2d5d39a15ac737d5b7080641ac2c76c9caa96506
nuggetbucket54 <ryan.zy.cheng@gmail.com>
2024-01-12 12:23:45 -0800

    formatting

--- models/payload_model.go
+++ models/payload_model.go
@@ -2,6 +2,6 @@ package models
 
 type Payload struct {
 	//To create a payload, ID of "-1" must be passed
-	ID             int           `json:"id,string" validate:"required" gorm:"primaryKey" example:"1" extensions:"x-order=1"`
-	Ground_Object  GroundObject  `json:"object" validate:"required" extensions:"x-order=2"`
+	ID             int          `json:"id,string" validate:"required" gorm:"primaryKey" example:"1" extensions:"x-order=1"`
+	GroundObject  GroundObject  `json:"ground_object" validate:"required" gorm:"-" extensions:"x-order=2"`
 }
