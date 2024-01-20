3c761f033ad47cea2b340f50ff5c08977b819419
nuggetbucket54 <ryan.zy.cheng@gmail.com>
2024-01-20 10:50:15 -0800

    removed unnecessary comments in payload controller and ground object controller

--- controllers/ground_object_controller.go
+++ controllers/ground_object_controller.go
@@ -22,28 +22,16 @@ import (
 //	@Failure		500			{object}	responses.ErrorResponse			"Internal Error Creating Ground Object"
 //	@Router			/ground_object [post]
 func CreateGroundObject(c echo.Context) error {
-	var ground_object models.GroundObject    //Declares an empty Ground Object class
-	db, _ := c.Get("db").(*gorm.DB) //Obtains the DB instance from the context
+	var ground_object models.GroundObject    
+	db, _ := c.Get("db").(*gorm.DB) 
 
 	if err := c.Bind(&ground_object); err != nil {
-		/*
-			.Bind() basically tries to force the JSON data provided into the
-			struct, using the json:"field" annotations we added to know what
-			goes where. c.Bind directly edits the ground object variable and as such
-			we provide the memory address of it using the & symbol in front.
-			c.Bind returns an error if something goes wrong, and we catch it here
-			by checking if it is nil
-		*/
 		return c.JSON(http.StatusBadRequest, responses.ErrorResponse{
 			Message: "Invalid JSON format",
 			Data:    err.Error()})
 	}
 
 	if validationErr := validate.Struct(&ground_object); validationErr != nil {
-		/*
-			validate.Struct() validates the struct based on the validate
-			annotation we provided in the struct definition
-		*/
 		return c.JSON(http.StatusBadRequest, responses.ErrorResponse{
 			Message: "Invalid ground object data",
 			Data:    validationErr.Error()})
@@ -57,10 +45,6 @@ func CreateGroundObject(c echo.Context) error {
 	}
 
 	if createErr := db.Create(&ground_object).Error; createErr != nil {
-		/*
-			Here we use GROM functions. GROM has already created tables for the
-			model definitions we provided and knows what type `ground_object` is
-		*/
 		return c.JSON(http.StatusInternalServerError, responses.ErrorResponse{
 			Message: "An error occurred creating the ground object"})
 	}
--- controllers/payload_contoller.go
+++ controllers/payload_contoller.go
@@ -22,28 +22,16 @@ import (
 //	@Failure		500			{object}	responses.ErrorResponse		"Internal Error Payload"
 //	@Router			/payload [post]
 func CreatePayload(c echo.Context) error {
-	var payload models.Payload    //Declares an empty Payload class
-	db, _ := c.Get("db").(*gorm.DB) //Obtains the DB instance from the context
+	var payload models.Payload   
+	db, _ := c.Get("db").(*gorm.DB) 
 
 	if err := c.Bind(&payload); err != nil {
-		/*
-			.Bind() basically tries to force the JSON data provided into the
-			struct, using the json:"field" annotations we added to know what
-			goes where. c.Bind directly edits the payload variable and as such
-			we provide the memory address of it using the & symbol in front.
-			c.Bind returns an error if something goes wrong, and we catch it here
-			by checking if it is nil
-		*/
 		return c.JSON(http.StatusBadRequest, responses.ErrorResponse{
 			Message: "Invalid JSON format",
 			Data:    err.Error()})
 	}
 
 	if validationErr := validate.Struct(&payload); validationErr != nil {
-		/*
-			validate.Struct() validates the struct based on the validate
-			annotation we provided in the struct definition
-		*/
 		return c.JSON(http.StatusBadRequest, responses.ErrorResponse{
 			Message: "Invalid payload data",
 			Data:    validationErr.Error()})
@@ -57,10 +45,6 @@ func CreatePayload(c echo.Context) error {
 	}
 
 	if createErr := db.Create(&payload).Error; createErr != nil {
-		/*
-			Here we use GROM functions. GROM has already created tables for the
-			model definitions we provided and knows what type `payload` is
-		*/
 		return c.JSON(http.StatusInternalServerError, responses.ErrorResponse{
 			Message: "An error occurred creating the payload"})
 	}
