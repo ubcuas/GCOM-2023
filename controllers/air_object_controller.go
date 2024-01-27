70c153d7ad5f90989f86d1ac5cece84e5b4ded31
Jim Geng <jimgeng2004@gmail.com>
2024-01-27 15:40:05 -0800

    finish AirObject GET, PUT, DEL methods

--- controllers/air_object_controller.go
+++ controllers/air_object_controller.go
@@ -1,19 +1,103 @@
 package controllers
 
 import (
+	"gcom-backend/models"
+	"gcom-backend/responses"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
 )
 
 // GetAirObjects gets all present air objects (drones)
 //
-//	@Summary		Get drone status
-//	@Description	Get the current status of the drone
-//	@Tags			Drone
+//	@Summary		Get air objects
+//	@Description	Get the status of all current air objects (drones)
+//	@Tags			AirObject
 //	@Produce		json
 //	@Success		200	{object}	models.Drone	"Success"
-//	@Router			/status [get]
+//	@Failure		404	{object}	responses.ErrorResponse "Objects not found"
+//	@Failure		500	{object}	responses.ErrorResponse "Internal error querying AirObjects"
+//	@Router			/air_object [get]
 func GetAirObjects(c echo.Context) error {
-	return c.JSON(http.StatusOK, c.Get("drone"))
+	var airObjects []models.AirObject
+	db, _ := c.Get("db").(*gorm.DB)
+
+	if err := db.Find(&airObjects).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, responses.ErrorResponse{
+			Message: "Internal error querying AirObjects",
+			Data:    err.Error(),
+		})
+	}
+
+	return c.JSON(http.StatusOK, responses.AirObjectsResponse{
+		Message:    "AirObjects found!",
+		AirObjects: airObjects,
+	})
+}
+
+// DeleteAirObjects deletes all present air objects (remoteID drones)
+//
+//	@Summary		Delete all AirObjects
+//	@Description	Delete all AirObjects (remoteID drones)
+//	@Tags			AirObject
+//	@Produce		json
+//	@Success		200	{object}	responses.WaypointResponse	"Success (returns a blank Waypoint)"
+//	@Failure		500	{object}	responses.ErrorResponse		"Internal Error Deleting Waypoint"
+//	@Router			/air_objects [delete]
+func DeleteAirObjects(c echo.Context) error {
+	db, _ := c.Get("db").(*gorm.DB)
+	// gorm doesn't support global batch deleting without a where clause
+	dbAction := db.Where("1 = 1").Delete(&models.AirObject{})
+	if dbAction.Error != nil {
+		return c.JSON(http.StatusInternalServerError, responses.ErrorResponse{
+			Message: "Internal error deleting AirObjects",
+			Data:    dbAction.Error.Error(),
+		})
+	}
+	return c.JSON(http.StatusOK, responses.AirObjectsResponse{
+		Message: "All AirObjects deleted!",
+	})
+}
+
+// CreateAirObjects creates new AirObjects (remoteID drones) from a json array.
+//
+//	@Summary		Create multiple AirObjects
+//	@Description	Create multiple AirObjects for remoteID based on JSON array.
+//	@Tags			AirObject
+//	@Accept			json
+//	@Produce		json
+//	@Param			waypoints	body		[]models.AirObject			true	"Array of Waypoint Data"
+//	@Success		200			{object}	responses.WaypointsResponse	"Success"
+//	@Failure		400			{object}	responses.ErrorResponse		"Invalid JSON or AirObject Data"
+//	@Failure		500			{object}	responses.ErrorResponse		"Internal Error Creating AirObjects"
+//	@Router			/air_object [post]
+func CreateAirObjects(c echo.Context) error {
+	var airObjects []models.AirObject
+	db, _ := c.Get("db").(*gorm.DB)
+
+	if err := c.Bind(&airObjects); err != nil {
+		return c.JSON(http.StatusBadRequest, responses.ErrorResponse{
+			Message: "Invalid JSON format",
+			Data:    err.Error()})
+	}
+
+	for _, airObject := range airObjects {
+		if validationErr := validate.Struct(airObject); validationErr != nil {
+			return c.JSON(http.StatusBadRequest, responses.ErrorResponse{
+				Message: "Invalid AirObject format",
+				Data:    validationErr.Error()})
+		}
+	}
+
+	if createErr := db.Create(&airObjects).Error; createErr != nil {
+		return c.JSON(http.StatusInternalServerError, responses.ErrorResponse{
+			Message: "Internal error creating AirObjects",
+			Data:    createErr.Error()})
+	}
+
+	return c.JSON(http.StatusOK, responses.AirObjectsResponse{
+		Message:    "AirObjects created!",
+		AirObjects: airObjects,
+	})
 }
--- models/air_object_model.go
+++ models/air_object_model.go
@@ -1,12 +1,15 @@
 package models
 
+// AirObject describes the information from remoteID of other present drones.
+//
+// @Description describes the information from remoteID of other present drones.
 type AirObject struct {
-	ID            string  `json:"id"`
-	Longitude     float64 `json:"longitude"`
-	Latitude      float64 `json:"latitude"`
-	Altitude      float64 `json:"altitude"`
-	VerticalSpeed float64 `json:"v_speed"`
-	Speed         float64 `json:"speed"`
-	Heading       float64 `json:"heading"`
-	Timestamp     int     `json:"timestamp"`
+	ID            string  `json:"id" gorm:"primaryKey" validate:"required" example:"FIN87astrdge12k8"`
+	Longitude     float64 `json:"longitude" validate:"required" example:"49.260605"`
+	Latitude      float64 `json:"latitude" validate:"required" example:"-123.245995"`
+	Altitude      float64 `json:"altitude" validate:"required" example:"75"`
+	VerticalSpeed float64 `json:"v_speed" validate:"required" example:"-1.2"`
+	Speed         float64 `json:"speed" validate:"required" example:"0.56"`
+	Heading       float64 `json:"heading" validate:"required" example:"12"`
+	Timestamp     int     `json:"timestamp" validate:"required" example:"1700905713"`
 }
