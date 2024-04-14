b1eeaa989230abdfd923a00ef667a613966f528e
Jim Geng <jimgeng2004@gmail.com>
2024-04-14 00:27:25 -0700

    add socket telemetry boilerplate

--- controllers/websocket_controller.go
+++ controllers/websocket_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/zishang520/socket.io/v2/socket"
@@ -14,12 +15,45 @@ func WebsocketHandler() echo.HandlerFunc {
 		fmt.Println("connected client")
 		client := clients[0].(*socket.Socket)
 
+		var telemetryTicker *time.Ticker
+		var stopChannel chan bool
+
 		client.On("ping", func(a ...any) {
 			client.Emit("pong")
 		})
 
 		client.On("disconnect", func(a ...any) {
 			fmt.Println("disconnected client")
+		})
+
+		client.On("telemetry_start", func(a ...any) {
+			if telemetryTicker != nil {
+				return
+			}
+			fmt.Println("telemetry_start")
+			telemetryTicker = time.NewTicker(500 * time.Millisecond)
+			stopChannel = make(chan bool)
+			go func() {
+				for {
+					select {
+					case <-telemetryTicker.C:
+						client.Emit("telemetry_data", "DATAHERE")
+					case <-stopChannel:
+						return
+					}
+				}
+			}()
+		})
+
+		client.On("telemetry_stop", func(a ...any) {
+			if telemetryTicker == nil {
+				return
+			}
+			fmt.Println("telemetry_stop")
+			telemetryTicker.Stop()
+			stopChannel <- true
+			close(stopChannel)
+			telemetryTicker = nil
 		})
 	})
 
