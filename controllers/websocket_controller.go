62f96b926ba8656f731a75fa5391b3647e4ff9d5
Aden Chan <40689649+21chanas3@users.noreply.github.com>
2024-05-04 21:46:53 -0700

    fixed sockets

--- controllers/websocket_controller.go
+++ controllers/websocket_controller.go
@@ -1,59 +1,54 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
-	"time"
-
+	"gcom-backend/models"
 	"github.com/labstack/echo/v4"
 	"github.com/zishang520/socket.io/v2/socket"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"time"
 )
 
-func WebsocketHandler() echo.HandlerFunc {
+func WebsocketHandler() func(context echo.Context) error {
 	io := socket.NewServer(nil, nil)
+	db, err := gorm.Open(sqlite.Open("./db/database.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
 
 	io.On("connection", func(clients ...any) {
-		fmt.Println("connected client")
+		fmt.Println("[SOCKET] Client Connected")
 		client := clients[0].(*socket.Socket)
-
-		var telemetryTicker *time.Ticker
-		var stopChannel chan bool
 
 		client.On("ping", func(a ...any) {
 			client.Emit("pong")
 		})
 
 		client.On("disconnect", func(a ...any) {
-			fmt.Println("disconnected client")
+			fmt.Println("[SOCKET] Client Disconnected")
 		})
 
-		client.On("telemetry_start", func(a ...any) {
-			if telemetryTicker != nil {
-				return
+		client.On("drone_update", func(a ...any) {
+			droneMap := a[0].(map[string]interface{})
+			jsonString, err := json.Marshal(droneMap)
+			if err != nil {
+				client.Emit("error", err.Error())
 			}
-			fmt.Println("telemetry_start")
-			telemetryTicker = time.NewTicker(500 * time.Millisecond)
-			stopChannel = make(chan bool)
-			go func() {
-				for {
-					select {
-					case <-telemetryTicker.C:
-						client.Emit("telemetry_data", "DATAHERE")
-					case <-stopChannel:
-						return
-					}
-				}
-			}()
-		})
 
-		client.On("telemetry_stop", func(a ...any) {
-			if telemetryTicker == nil {
-				return
+			var drone models.Drone
+			// Read received drone JSON
+			if err := json.Unmarshal(jsonString, &drone); err != nil {
+				client.Emit("error", err.Error())
+			} else {
+				fmt.Println(drone)
+				//Add drone
+				db.Save(&drone)
+				//Delete drones older than 5 minutes
+				var drones []models.Drone
+				db.Delete(&drones, "timestamp < ?", time.Now().Unix()-300)
 			}
-			fmt.Println("telemetry_stop")
-			telemetryTicker.Stop()
-			stopChannel <- true
-			close(stopChannel)
-			telemetryTicker = nil
 		})
 	})
 
--- models/drone_model.go
+++ models/drone_model.go
@@ -4,12 +4,12 @@ package models
 //
 // @Description describes the drone being flown
 type Drone struct {
-	Timestamp     int64     `json:"timestamp" gorm:"primaryKey" validate:"required" example:"1698544781" extensions:"x-order=1"`
-	Latitude      float64 `json:"lat" validate:"required" example:"49.267941" extensions:"x-order=2"`
-	Longitude     float64 `json:"long" validate:"required" example:"-123.247360" extensions:"x-order=3"`
-	Altitude      float64 `json:"alt" validate:"required" example:"100.00" extensions:"x-order=4"`
-	VerticalSpeed float64 `json:"v_speed" validate:"required" example:"-1.63" extensions:"x-order=5"`
-	Speed         float64 `json:"speed" validate:"required" example:"0.98" extensions:"x-order=6"`
+	Timestamp     int64   `json:"timestamp" gorm:"primaryKey" validate:"required" example:"1698544781" extensions:"x-order=1"`
+	Latitude      float64 `json:"latitude" validate:"required" example:"49.267941" extensions:"x-order=2"`
+	Longitude     float64 `json:"longitude" validate:"required" example:"-123.247360" extensions:"x-order=3"`
+	Altitude      float64 `json:"altitude" validate:"required" example:"100.00" extensions:"x-order=4"`
+	VerticalSpeed float64 `json:"vertical_velocity" validate:"required" example:"-1.63" extensions:"x-order=5"`
+	Speed         float64 `json:"velocity" validate:"required" example:"0.98" extensions:"x-order=6"`
 	Heading       float64 `json:"heading" validate:"required" example:"298.12" extensions:"x-order=7"`
 	//Payloads TBD
 	BatteryVoltage float64 `json:"battery_voltage" validate:"required" example:"2.6" extensions:"x-order=9"`
